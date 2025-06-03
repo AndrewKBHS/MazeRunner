703eae92904ddc7dc5632d137cb69c1acf5d7eb7
Andrew Kim <111266387+AndrewKBHS@users.noreply.github.com>
2025-06-02 23:24:59 -0700

    Update Blob.java

--- src/Blob.java
+++ src/Blob.java
@@ -1,7 +1,15 @@
+/**
+    *Creates a blob runner
+*/
 public class Blob extends Runner {
-    private String shape;
-    private String[] moves;
+    private String shape; 
+    private String[] moves; 
 
+    /**
+        *Creates a blob character
+        *@param shape the blob as a string
+        *@param moves blob collision animation sequence
+    */
     public Blob() {
         super("o", new String[] {"*","o","O","8", "O", "o", "O", "8","O","o","O","8","O","o"});;
     }
