65177ecb836d6d0d4a7c19c23097ee66f99ad3a0
Andrew Kim <111266387+AndrewKBHS@users.noreply.github.com>
2025-06-02 23:26:48 -0700

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,22 +1,31 @@
 /*
-    *Stores a runner object
+    *Creaters a human runner
 */
 public class Runner {
     private String shape;
     private String[] moves;
 
+   /**
+      *Creates a human
+      *@param shape the human as a string
+      *@param moves the human falling animatikon
+   */
     public Runner(String shape, String[] moves) {
         this.shape = shape;
         this.moves = moves;
     }
 
+   /**
+      *Returns the human runner as a string
+   */
     public String toString() {
         return shape;
     }
 
+   /**
+      *Returns the human falling animation sequence
+   */
     public String[] getMoves() {
         return moves;
     }
-
-
 }
